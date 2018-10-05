8f1a56f595ebc49800c28df7c76226c558b253d9
Ruben Perez Rubio <rubenperezrubio@gmail.com>
2018-10-05 21:07:57 +0200

    Primer Commit y push de prueba

---
+++ AvengersLive/src/main/java/AvengersLive/AvengersLive/App.java
@@ -0,0 +1,13 @@
+package AvengersLive.AvengersLive;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ AvengersLive/src/main/java/AvengersLive/AvengersLive/Prueba.java
@@ -0,0 +1,10 @@
+package AvengersLive.AvengersLive;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola Mundo");
+	}
+
+}
